e01e63a00f1e55d84cc6858a08e97ce9e4cfc4ea
Hirose-Yuto <hirose.y.aj@m.titech.ac.jp>
2023-05-16 02:24:38 +0900

    write object

--- src/app/tools/objectService.go
+++ src/app/tools/objectService.go
@@ -1,8 +1,14 @@
 package tools
 
 import (
+	"bytes"
+	"compress/zlib"
+	"crypto/sha1"
+	"encoding/hex"
+	"io"
 	"os"
 	"path"
+	"strconv"
 )
 
 type ObjectService struct {
@@ -13,12 +19,57 @@ func NewObjectService(repoPath string) *ObjectService {
 	return &ObjectService{repoPath: repoPath}
 }
 
-func (oS *ObjectService) ReadBlob(objectID string) ([]byte, error) {
+func (oS *ObjectService) ReadBlob(objectID string) (string, error) {
 	bs, err := os.ReadFile(oS.getObjectPath(objectID))
 	if err != nil {
+		return "", err
+	}
+	return oS.zlibBytesToString(bs)
+}
+
+func (oS *ObjectService) WriteBlob(content string) (string, error) {
+	blobData := "blob " + strconv.Itoa(len([]byte(content))) + "\000" + content
+	sh := sha1.New()
+	if _, err := io.WriteString(sh, blobData); err != nil {
+		return "", err
+	}
+	objectID := hex.EncodeToString(sh.Sum(nil))
+
+	objectPath := oS.getObjectPath(objectID)
+	if err := os.MkdirAll(path.Dir(objectPath), os.ModePerm); err != nil && !os.IsExist(err) {
+		return "", err
+	}
+
+	zlibData, err := oS.stringToZlibBytes(blobData)
+	if err != nil {
+		return "", nil
+	}
+	if err := os.WriteFile(objectPath, zlibData, os.ModePerm); err != nil {
+		return "", err
+	}
+	return objectID, nil
+}
+
+func (oS *ObjectService) zlibBytesToString(bs []byte) (string, error) {
+	extract, err := zlib.NewReader(bytes.NewReader(bs))
+	if err != nil {
+		return "", err
+	}
+	buf := bytes.Buffer{}
+	_, err = buf.ReadFrom(extract)
+	return string(buf.Bytes()), nil
+}
+
+func (oS *ObjectService) stringToZlibBytes(content string) ([]byte, error) {
+	var b bytes.Buffer
+	w := zlib.NewWriter(&b)
+	if _, err := w.Write([]byte(content)); err != nil {
 		return nil, err
 	}
-	return bs, nil
+	if err := w.Close(); err != nil {
+		return nil, err
+	}
+	return b.Bytes(), nil
 }
 
 func (oS *ObjectService) getObjectPath(objectID string) string {
--- src/test.go
+++ src/test.go
@@ -1,5 +1,43 @@
 package main
 
+import (
+	"fmt"
+	"main/app/tools"
+	"os"
+)
+
 func main() {
-	//oS := tools.NewObjectService("/e")
+	oS := tools.NewObjectService("exec/.git")
+
+	readFile(oS)
+
+	//writeFile(oS)
+
+	//sh := sha1.New()
+	//io.WriteString(sh, "blob 5\000hello")
+	//fmt.Println(hex.EncodeToString(sh.Sum(nil)))
+}
+
+func readFile(oS *tools.ObjectService) {
+	content, err := oS.ReadBlob("5692df1010ac406517147fa6976ca6e8d15721ec")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(content)
+	fmt.Println([]byte(content))
+}
+
+func writeFile(oS *tools.ObjectService) {
+	bs, err := os.ReadFile("go.mod")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	blob, err := oS.WriteBlob(string(bs))
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+	fmt.Println(blob)
 }
